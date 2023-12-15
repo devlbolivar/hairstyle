19ae52d315b6250452012f0af84ee89fea3ffb3f
Luis <luisbolivar515@gmail.com>
2023-12-15 00:15:13 -0300

    Add connections

---
+++ backend/models/productModel.js
@@ -0,0 +1,110 @@
+import mongoose from "mongoose";
+
+const reviewSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: [true, "Please enter your name"],
+    },
+    rating: {
+      type: Number,
+      required: [true, "Please enter product rating"],
+    },
+    comment: {
+      type: String,
+      required: [true, "Please enter your comment"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const productSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  name: {
+    type: String,
+    required: [true, "Please enter product name"],
+    trim: true,
+    maxLength: [100, "Product name cannot exceed 100 characters"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Please enter product price"],
+    maxLength: [5, "Product name cannot exceed 5 characters"],
+    default: 0.0,
+  },
+  description: {
+    type: String,
+    required: [true, "Please enter product description"],
+  },
+  ratings: {
+    type: Number,
+    default: 0,
+  },
+  images: [
+    {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  category: {
+    type: String,
+    required: [true, "Please select category for this product"],
+    enum: {
+      values: [
+        "Electronics",
+        "Cameras",
+        "Laptop",
+        "Accessories",
+        "Headphones",
+        "Food",
+        "Books",
+        "Clothes/Shoes",
+        "Beauty/Health",
+        "Sports",
+        "Outdoor",
+        "Home",
+      ],
+      message: "Please select correct category for product",
+    },
+  },
+  seller: {
+    type: String,
+    required: [true, "Please enter product seller"],
+  },
+  stock: {
+    type: Number,
+    required: [true, "Please enter product stock"],
+    maxLength: [5, "Product name cannot exceed 5 characters"],
+    default: 0,
+  },
+  numOfReviews: {
+    type: Number,
+    default: 0,
+  },
+  reviews: [reviewSchema],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
---
+++ backend/models/userModel.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter your name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter your email"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter your password"],
+    minLength: [6, "Your password must be longer than 6 characters"],
+    select: false,
+  },
+  isAdming: {
+    type: Boolean,
+    default: false,
+  },
+  avatar: {
+    public_id: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
